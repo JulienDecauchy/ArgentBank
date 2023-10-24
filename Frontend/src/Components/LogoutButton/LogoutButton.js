0270c5ba37348c04efb9c543dc41b3298cc28d28
JulienDecauchy <julien.decauchy1@gmail.com>
2023-10-24 12:12:45 +0200

    Update Logout Button
    - Create Logout condition
    - Add bridge between "Stores" and Logout

--- Frontend/src/Components/LogoutButton/LogoutButton.js
+++ Frontend/src/Components/LogoutButton/LogoutButton.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStore } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { logoutUser } from "../../store/userStore";
 
 
 function LogoutButton() {
+    const [userName, setUserName] = useState()
+
+    const store = useStore()
+    store.subscribe(() => setUserName(store.getState()?.user?.data?.firstName))
+
+    const Logout = () => {
+        logoutUser(store)
+    }
 
     return (
         <div className="main-nav-logout">
-            
+            <NavLink className="main-nav-item" to="/User">
+                <i class="fa-solid fa-right-from-bracket">{userName}</i>
+            </NavLink>
+            <NavLink className="main-nav-item" onClick={Logout} to="/">Sign Out</NavLink>
         </div>
     )
 }
+
+export default LogoutButton
