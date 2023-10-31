f7d0c7b4589b64a88954de261c08cc048a7c865b
JulienDecauchy <julien.decauchy1@gmail.com>
2023-10-31 11:23:55 +0100

    Update API - Logout & UserName
    - Fixed Typo in Login API
    - Add Logout and change UserName API

--- Frontend/src/Redux/userAPI.js
+++ Frontend/src/Redux/userAPI.js
@@ -16,11 +16,42 @@ export const login = (email, password) => async (dispatch) => {
             });
         } else {
             console.error(
-                "Erreur lors de la connexion. Statut de réponse:",
+                "Login Error. Response Status:",
                 response.status
             );
         }
     } catch (error) {
         console.error("Internal Error", error);
+    }
+};
+
+export const logout = () => ({ type: "LOGOUT" });
+
+export const userName = (newUserName) => async (dispatch, getState) => {
+    try {
+        const { token } = getState();
+        const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+            method: "PUT",
+            body: JSON.stringify({ userName: newUserName }),
+            headers: {
+                accept: "application/json",
+                Authorization: `Bearer ${token.token}`,
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            dispatch({
+                type: "USER_NAME",
+                payload: newUserName,
+            });
+        } else {
+            console.error(
+                "Error when updating username. Response Status:",
+                response.status
+            );
+        }
+    } catch (error) {
+        console.error("Error when updating username", error);
     }
 };
