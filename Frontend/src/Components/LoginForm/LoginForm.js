92ae3573049131bdbcb633379cf93aaf902a1e62
JulienDecauchy <julien.decauchy1@gmail.com>
2023-10-31 11:50:37 +0100

    Update - LoginForm and Button
    - Remove unnecessary component
    - Adapt LoginForm synthax to new Slice and Store

--- Frontend/src/Components/LoginForm/LoginForm.js
+++ Frontend/src/Components/LoginForm/LoginForm.js
@@ -1,45 +1,82 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import ActivateButton from "../ActivateButton/ActivateButton";
-import { Login } from "../../Redux/userAPI";
+import { login } from "../../Redux/userAPI";
 import { getProfile } from "../../Redux/userAPI";
 import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 function LoginForm() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const isAuthenticated = useSelector((state) => state.isAuthenticated);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAuthenticated = useSelector((state) => state.isAuthenticated);
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState('')
+  const [rememberMe, setRememberMe] = useState(false);
 
-    return (
-        <div className="form">
-        <div className="input-wrapper">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="input-remember">
-          <input type="checkbox" id="remember-me" />
-          <label htmlFor="remember-me">Remember me</label>
-        </div>
-        <ActivateButton title="Sign In" action={Form}/>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      console.error("Erreur interne");
+      return;
+    }
+
+    const handleRememberMe = (rememberMe, username, password) => {
+      if (rememberMe) {
+        Cookies.set(
+          "rememberMe",
+          JSON.stringify({ username, password }, { expires: 7 }),
+          {
+            secure: true,
+            sameSite: "strict",
+          }
+        );
+      } else {
+        Cookies.remove("rememberMe");
+      }
+    };
+
+    handleRememberMe(rememberMe, username, password);
+
+    dispatch(login(username, password));
+  };
+
+
+  return (
+    <div className="form">
+      <div className="input-wrapper">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
-    )
-  }
-  
-  export default LoginForm
+      <div className="input-wrapper">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="input-remember">
+        <input 
+          type="checkbox" 
+          id="remember-me" 
+          checked={rememberMe}
+          onChange={() => setRememberMe(!rememberMe)}
+        />
+        <label htmlFor="remember-me">Remember me</label>
+      </div>
+      <button type="submit" className="transaction-button">
+          Sign In
+        </button>
+    </div>
+  )
+}
+
+export default LoginForm
