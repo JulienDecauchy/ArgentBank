8b49dbf8aa73849fad5060edb9c3d757f5164c45
JulienDecauchy <julien.decauchy1@gmail.com>
2023-11-10 23:04:57 +0100

    Export account data in another component

---
+++ Frontend/src/Components/Account-Data/account-data.js
@@ -0,0 +1,19 @@
+const accountData = [
+    {
+        title: "Argent Bank Checking (x8349)",
+        content: "$2,082.79",
+        description: "Available Balance",
+    },
+    {
+        title: "Argent Bank Savings (x6712)",
+        content: "$10,928.42",
+        description: "Available Balance",
+    },
+    {
+        title: "Argent Bank Credit Card (x8349)",
+        content: "$184.30",
+        description: "Current Balance",
+    },
+];
+
+export default accountData
